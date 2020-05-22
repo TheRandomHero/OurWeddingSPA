1b6c37b516d281360486cd7ba73a82ca19b57a0d
Attila Molnar <scythianhawk@gmail.com>
2020-05-22 13:12:54 +0200

    drawer image fixed and added 3 button to bot

--- wedding-web-page/src/components/NavBar.vue
+++ wedding-web-page/src/components/NavBar.vue
@@ -1,22 +1,22 @@
 <template>
-    <v-container fluid>
-        <v-navigation-drawer app permanent width="350" src="./../assets/ring.jpg">
-            <v-list nav class="pa-0">
-                <div class="drawer-header">
-                    <router-link to="/">
-                        <p>Melinda</p>
-                            <span>És</span>
-                        <p>Attila</p>
-                    </router-link>
-                </div>
-                <v-list-item v-for="(item,i) in navItems" :key="i" router :to="item.route">
-                    <v-list-item-content>
-                        <v-list-item-title>{{ item.text }}</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
-    </v-container>
+    <v-navigation-drawer app permanent width="350" height="100%">
+        <v-img src="./../assets/ring.jpg"  class="drawer-img" height="100%" width="100%"> 
+            <div class="drawer-header">
+                <router-link to="/">
+                    <p>Melinda</p>
+                        <span>És</span>
+                    <p>Attila</p>
+                </router-link>
+            </div>
+        <v-list nav class="pa-0">
+            <v-list-item v-for="(item,i) in navItems" :key="i" router :to="item.route">
+                <v-list-item-content>
+                    <v-list-item-title>{{ item.text }}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+        </v-img>
+    </v-navigation-drawer>
 </template>
 
 <script>
@@ -33,7 +33,4 @@ export default {
 }
 </script>
 <style scoped>
-.drawer-header{
-    background: brown;
-}
 </style>
--- wedding-web-page/src/views/Home.vue
+++ wedding-web-page/src/views/Home.vue
@@ -3,10 +3,27 @@
     <div class="home">
       <img width="100%" src="../assets/homepage/fp-picture4.png">
     </div>
-    <div>
-      <Countdown end="September 26, 2020"></Countdown>
-    </div>
-
+    <v-row>
+      <v-col>
+        <p>2020.</p>
+        <p>szeptember 26.</p>
+      <div>
+        <Countdown end="September 26, 2020"></Countdown>
+      </div>
+      </v-col>
+      <v-col>
+        <p>Tandari Tanya, Martonos</p>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="8" offset="2">
+        <v-row justify="space-between">
+          <v-btn class="mx-2" href="/aboutUs">Rólunk</v-btn>
+          <v-btn class="mx-2" href="/info">További információ</v-btn>
+          <v-btn class="mx-2" href="/gallery">Galéria megtekintése</v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
