9657824ac8a2adfdd3eb54d20c088acf73934207
Attila Molnar <scythianhawk@gmail.com>
2020-05-24 18:14:37 +0200

    static files added

--- wedding-web-page/src/main.js
+++ wedding-web-page/src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import * as VueGoogleMaps from 'vue2-google-maps' 
+
+require('dotenv').config()
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps,{
+  load:{
+    key: process.env.VUE_APP_MAPKEY,
+    libraries: 'places'
+
+  }
+})
 
 new Vue({
   router,
--- wedding-web-page/src/router/index.js
+++ wedding-web-page/src/router/index.js
@@ -21,10 +21,12 @@ const routes = [
   {
     path: '/gallery',
     name: 'Gallery',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Gallery.vue')
+    component: () => import('../views/Gallery.vue')
+  },
+  {
+    path: '/info',
+    name: 'Information',
+    component: () => import('../views/Information.vue')
   }
 ]
 
