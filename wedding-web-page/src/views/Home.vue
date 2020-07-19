5fbf25e957d48bd94cad93d403a14cbb665381f7
Attila Molnar <scythianhawk@gmail.com>
2020-07-19 18:10:47 +0200

    mobile home pic added

--- wedding-web-page/src/views/Home.vue
+++ wedding-web-page/src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
   <v-container fluid  pa-0 ma-0>
       <div >
-        <img class="home" src="../assets/homepage/main_final.png">
+        <img class="home" :src="screenSize">
       </div>
+      <v-row>
+        <v-col>
+          <v-row justify="center">
+            <p>2020.
+            Szeptember 26.</p>
+          </v-row>
+        </v-col>
+      </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
+  computed:{
+    screenSize(){
+      return this.$vuetify.breakpoint.smAndDown ? require('../assets/homepage/mobile_main_final.png') : require('../assets/homepage/main_final.png')
+    }
+  }
 }
 </script>
 
