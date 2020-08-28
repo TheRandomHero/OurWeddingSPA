8b3b207b799e05ecff0ea8ee409610d0295ee1b1
unknown <scythianhawk@gmail.com>
2020-08-28 11:46:10 +0200

    canceld stamp added to date, and new date revealed

--- wedding-web-page/src/views/Home.vue
+++ wedding-web-page/src/views/Home.vue
@@ -6,8 +6,14 @@
       <v-row>
         <v-col>
           <v-row justify="center">
-            <p class="date">2020.
-            Szeptember 26.</p>
+            <div class="date stamp">2020.
+            Szeptember 26.</div>
+          </v-row>
+          <v-row justify="center">
+            <div class="new-date">
+              Az új dátum: <br>
+              2021. Június 25.
+            </div>
           </v-row>
         </v-col>
       </v-row>
@@ -28,12 +34,43 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Pattaya');
   .home{
     padding: 0;
-    display: block;
     width: 100%;
   }
 .date{
   font-family: 'Pattaya';
   font-size: 2em;
+  display:inline-block;
+  vertical-align:top;
+  box-sizing:border-box;
+  position:relative;
 }
 
+.new-date{
+  font-family: 'Pattaya';
+  font-size: 2em;
+  display:inline-block;
+  position:relative;
+  margin-top: 40px;
+  text-align: center;
+}
+
+.stamp::after {
+  content: "ELHALASZTVA";
+  position: absolute;
+  z-index: 1;
+  transform: rotate(12deg);
+	font-size: 2rem;
+	font-weight: 700;
+  color: #D23;
+  border: 0.5rem double #D23;
+	display: block;
+  top: -10px;
+  top: 0px;
+	padding: 0.25rem 1rem;
+	text-transform: uppercase;
+	border-radius: 1rem;
+	font-family: 'Courier';
+	-webkit-mask-image: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/8399/grunge.png');
+  -webkit-mask-size: 944px 604px;
+}
 </style>
